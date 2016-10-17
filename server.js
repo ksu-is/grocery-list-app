27886472977b15a3ecff2c2ddda4456056cb61a1
Zarela Velasquez <zarelavp@yahoo.com>
2016-10-17 16:32:09 -0400

    server.js completed

---
+++ server.js
@@ -0,0 +1,58 @@
+//MODULES
+//=========================================
+pry = require('pryjs')
+var express = require('express');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var methodOveride = require('method-override');
+var LocalStrategy = require('passport-local').Strategy;
+mongoose.Promise = global.Promise;
+var port  = 3000 || process.env.PORT //Check on this
+var app = express();
+var path    = require('path');
+
+//DATABASE CONNECTION
+//=================================
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/grocery-list');
+
+// MIDDLEWARE / CONFIGURATION
+// ==================================
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('public')); //Not sure if we should keep this one or the one above
+
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+//PASSPORT
+//=================================
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+//To be used when user is created ********
+// passport.use(User.createStrategy());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+
+//PORT LISTENER
+//=========================================
+app.listen(port, function() {
+    console.log('=======================');
+    console.log('RUNNING on PORT ' + port);
+    console.log('=======================');
+});
